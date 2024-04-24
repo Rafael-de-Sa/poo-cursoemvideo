e3b346055bdb5fb688e2943c785b8c72919bf785
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-24 16:40:41 -0300

    :sparkles: feat: ControleRemoto

---
+++ Aula06/src/aula06/ControleRemoto.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula06;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class ControleRemoto {
+
+
+}
