b9815e88b3fd2c34b4516afd2002289d87000a3a
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-24 16:41:43 -0300

    :sparkles: feat: Getters e Setters

--- Aula06/src/aula06/ControleRemoto.java
+++ Aula06/src/aula06/ControleRemoto.java
@@ -20,4 +20,28 @@ public class ControleRemoto {
         this.tocando = false;
     }
 
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean isLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean isTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
+
 }
