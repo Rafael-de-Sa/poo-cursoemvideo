9337970aba6b60961dc5a122d9e2b13e167ea0e9
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-24 23:15:05 -0300

    :test_tube: test: Chamando Métodos

--- Aula06/src/aula06/Aula06.java
+++ Aula06/src/aula06/Aula06.java
@@ -15,6 +15,12 @@ public class Aula06 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.maisVolume();
+
+        c.abrirMenu();
     }
-    
+
 }
