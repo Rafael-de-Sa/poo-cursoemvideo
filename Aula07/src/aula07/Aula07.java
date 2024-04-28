7872cee96a6ce909736b877f5d54debb28befc7b
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-28 10:53:10 -0300

    :test_tube: test: Instanciando Lutador

--- Aula07/src/aula07/Aula07.java
+++ Aula07/src/aula07/Aula07.java
@@ -11,6 +11,18 @@ package aula07;
 public class Aula07 {
 
     public static void main(String[] args) {
+        Lutador[] l = new Lutador[6];
+        l[0] = new Lutador("Pretty Boy", "França", 1.75f, 68.9f, 31, 11, 3, 1);
+        l[1] = new Lutador("Putscript", "Brasil", 1.68f, 57.8f, 29, 14, 2, 3);
+        l[2] = new Lutador("Snapshadow", "EUA", 1.65f, 80f, 35, 12, 2, 1);
+        l[3] = new Lutador("Dead Code", "Australia", 1.93f, 81.6f, 28, 13, 0, 2);
+        l[4] = new Lutador("Ufocobol", "Brasil", 1.7f, 119.3f, 37, 5, 4, 3);
+        l[5] = new Lutador("Nerdard", "EUA", 1.81f, 105.7f, 30, 12, 2, 4);
 
+        l[0].apresentar();
+        l[2].status();
+        l[3].getCategoria();
+        l[1].ganharLuta();
+        l[0].empatarLuta();
     }
 }
