b5902ee51ef9fe6b97890f84bc28ebb9eedad126
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-28 17:28:16 -0300

    :recycle: refactor: Alterando método de atribuição de desafiante e desafiado

--- Aula07/src/aula07/Luta.java
+++ Aula07/src/aula07/Luta.java
@@ -11,19 +11,21 @@ import java.util.Random;
  * @author Rafael de Sá <rafaeldesa170622@gmail.com>
  */
 public class Luta implements ILuta {
-
+    
     private Lutador desafiado, desafiante;
     private int rounds;
     private boolean aprovada;
-
+    
     @Override
     public void marcarLuta(Lutador desafiante, Lutador desafiado) {
+        this.setDesafiante(desafiante);
+        this.setDesafiado(desafiado);
+
         /*Pode ser marcada somente por lutadores da mesma categoria.
         *O desafiado e desafiante devem ser jogadores diferentes.
         *Só pode ser se for aprovada.
         *Só pode ter como resultado a vitória de um lutador ou empate.
          */
-
         if (this.desafiante.getCategoria().equals(this.desafiado.getCategoria()) && this.desafiante != this.desafiado) {
             this.aprovada = true;
         } else {
@@ -32,16 +34,16 @@ public class Luta implements ILuta {
             this.desafiado = null;
         }
     }
-
+    
     @Override
     public void lutar() {
         if (this.aprovada) {
             this.desafiado.apresentar();
             this.desafiante.apresentar();
-
+            
             Random random = new Random();
             int vencedor = random.nextInt(3);
-
+            
             switch (vencedor) {
                 case 0: //Empate
                     System.out.println("Empatou");
@@ -61,42 +63,42 @@ public class Luta implements ILuta {
                 default:
                     System.out.println("Resultado Inválido");
             }
-
+            
         } else {
             System.out.println("A luta não pode acontecer.");
         }
     }
-
+    
     public Lutador getDesafiado() {
         return desafiado;
     }
-
+    
     public void setDesafiado(Lutador desafiado) {
         this.desafiado = desafiado;
     }
-
+    
     public Lutador getDesafiante() {
         return desafiante;
     }
-
+    
     public void setDesafiante(Lutador desafiante) {
         this.desafiante = desafiante;
     }
-
+    
     public int getRounds() {
         return rounds;
     }
-
+    
     public void setRounds(int rounds) {
         this.rounds = rounds;
     }
-
+    
     public boolean isAprovada() {
         return aprovada;
     }
-
+    
     public void setAprovada(boolean aprovada) {
         this.aprovada = aprovada;
     }
-
+    
 }
