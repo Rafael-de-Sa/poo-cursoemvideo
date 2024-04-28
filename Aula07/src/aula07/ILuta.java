a76ffba0009f95962d056d2a0030074ba7b09674
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-28 17:05:03 -0300

    :sparkles: feat: Interface ILuta

---
+++ Aula07/src/aula07/ILuta.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package aula07;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public interface ILuta {
+
+    public abstract void marcarLuta(Lutador desafiante, Lutador desafiado);
+
+    public abstract void lutar();
+}
