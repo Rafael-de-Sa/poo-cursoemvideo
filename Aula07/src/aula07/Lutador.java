5a78bf8e5c773ff47226cfd0192185a5720bc28b
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-28 09:38:35 -0300

    :sparkles: feat:  Criando Classe Lutador

---
+++ Aula07/src/aula07/Lutador.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula07;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Lutador implements ILutador {
+}
