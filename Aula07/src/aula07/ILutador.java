ff212de067e0d1a77597951888852cd57bd27b06
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-04-28 09:37:54 -0300

    :sparkles: feat: Adicionado Interface da classe Lutador

---
+++ Aula07/src/aula07/ILutador.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package aula07;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public interface ILutador {
+
+    public abstract void apresentar();
+
+    public abstract void status();
+
+    public abstract void ganharLuta();
+
+    public abstract void perderLuta();
+
+    public abstract void empatarLuta();
+}
