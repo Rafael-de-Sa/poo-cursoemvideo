93f42d638f81180f3d0349c1c28dd0c6b17ecda8
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-05 15:11:09 -0300

    :tada: init: Iniciando Projeto Livro

---
+++ ProjetoLivro/src/projetolivro/ProjetoLivro.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetolivro;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class ProjetoLivro {
+
+    public static void main(String[] args) {
+
+    }
+}
