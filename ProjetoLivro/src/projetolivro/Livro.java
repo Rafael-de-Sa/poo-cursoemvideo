d5708a928709f76e9e44373c7ccea06dd92b8478
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-05 15:25:23 -0300

    :sparkles: feat: Adicionando Getters e Setters

--- ProjetoLivro/src/projetolivro/Livro.java
+++ ProjetoLivro/src/projetolivro/Livro.java
@@ -30,4 +30,52 @@ public class Livro {
         this.leitor = leitor;
     }
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getTotPaginas() {
+        return totPaginas;
+    }
+
+    public void setTotPaginas(int totPaginas) {
+        this.totPaginas = totPaginas;
+    }
+
+    public int getPagAtual() {
+        return pagAtual;
+    }
+
+    public void setPagAtual(int pagAtual) {
+        this.pagAtual = pagAtual;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public void setAberto(boolean aberto) {
+        this.aberto = aberto;
+    }
+
+    public Pessoa getLeitor() {
+        return leitor;
+    }
+
+    public void setLeitor(Pessoa leitor) {
+        this.leitor = leitor;
+    }
+
 }
