41f907e8d9623a0e4abf03cf1842818164b38186
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-05 15:22:27 -0300

    :sparkles: feat: Adicionando Classe Pessoa

---
+++ ProjetoLivro/src/projetolivro/Pessoa.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetolivro;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Pessoa {
+}
