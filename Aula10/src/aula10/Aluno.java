10ddd33f172e4b5d0ab308b3a32a937dfe305a19
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-21 09:14:21 -0300

    :sparkles: feat: Getters e Setters

--- Aula10/src/aula10/Aluno.java
+++ Aula10/src/aula10/Aluno.java
@@ -13,4 +13,21 @@ public class Aluno extends Pessoa {
     private int matr;
     private String curso;
 
+
+    public int getMatr() {
+        return matr;
+    }
+
+    public void setMatr(int matr) {
+        this.matr = matr;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
 }
