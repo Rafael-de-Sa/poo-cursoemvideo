d4effefa7e0830e0dd9bae3b8cd81afc9969673d
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-21 08:45:48 -0300

    :sparkles: feat: Aula10

---
+++ Aula10/src/aula10/Aula10.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula10;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Aula10 {
+
+    public static void main(String[] args) {
+
+    }
+}
