bfcbd831d94fd9c0e930916ac6a33c787a623256
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-21 09:13:47 -0300

    :sparkles: feat: Classe Aluno

---
+++ Aula10/src/aula10/Aluno.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula10;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Aluno extends Pessoa {
+
+
+}
