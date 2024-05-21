524300f8536270c99691ce06140eaf38bf0bb7be
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-21 09:54:55 -0300

    :sparkles: feat: toString()

--- Aula10/src/aula10/Pessoa.java
+++ Aula10/src/aula10/Pessoa.java
@@ -42,4 +42,9 @@ public class Pessoa {
         this.sexo = sexo;
     }
 
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", idade=" + idade + ", sexo=" + sexo + '}';
+    }
+
 }
