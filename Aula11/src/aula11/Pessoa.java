f9905e8b196a61932e36f202f5740d25eea492a5
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-22 21:48:37 -0300

    :sparkles: feat: Classe Pessoa

---
+++ Aula11/src/aula11/Pessoa.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula11;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public abstract class Pessoa {
+
+
+}
