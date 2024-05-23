96760d06825ebdaa29a628d02f6ec334fd27f7c0
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:31:56 -0300

    :sparkles: feat: emitirSom()

--- Aula12/src/aula12/Cachorro.java
+++ Aula12/src/aula12/Cachorro.java
@@ -17,4 +17,10 @@ public class Cachorro extends Mamifero {
     public void abanarRabo() {
         System.out.println("Abanando o Rabo");
     }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Au! Au! Au!");
+    }
+
 }
