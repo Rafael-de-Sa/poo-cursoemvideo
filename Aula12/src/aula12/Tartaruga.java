22b10294b2774235a02cf99965b9ca6583994e96
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:23:11 -0300

    :sparkles: feat: Classe Tartaruga

---
+++ Aula12/src/aula12/Tartaruga.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula12;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Tartaruga extends Reptil {
+
+    @Override
+    public void locomover() {
+        System.out.println("Andando beeeem devagar");
+    }
+
+}
