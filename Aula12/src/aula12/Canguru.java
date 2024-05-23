fd395ea4c841856c6b5967215eba17b2a8463ba3
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:22:29 -0300

    :sparkles: feat: Classe Canguru

---
+++ Aula12/src/aula12/Canguru.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula12;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Canguru extends Mamifero {
+
+    public void usarBolsa() {
+        System.out.println("Usando bolsa");
+    }
+
+    @Override
+    public void locomover() {
+        System.out.println("Saltando");
+    }
+
+}
