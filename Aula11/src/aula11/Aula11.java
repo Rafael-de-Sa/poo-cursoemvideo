4f3519a9178c49973baf881cd89b8f3b30abb7be
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-22 22:07:02 -0300

    :test_tube: test: Testando Aluno e Bolsista

--- Aula11/src/aula11/Aula11.java
+++ Aula11/src/aula11/Aula11.java
@@ -19,6 +19,21 @@ public class Aula11 {
         v1.setIdade(22);
         v1.setSexo("M");
         System.out.println(v1.toString());
+
+        Aluno a1 = new Aluno();
+        a1.setNome("Cláudio");
+        a1.setMatricula(1111);
+        a1.setCurso("Informática");
+        a1.setIdade(16);
+        a1.setSexo("M");
+        a1.pagarMensalidade();
+
+        Bolsista b1 = new Bolsista();
+        b1.setMatricula(1112);
+        b1.setNome("Jubileu");
+        b1.setBolsa(12.5f);
+        b1.setSexo("M");
+        b1.pagarMensalidade();
     }
 
 }
