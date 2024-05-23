26c04fb23bc3c499af72d72473f9df5915fd9bd2
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:21:58 -0300

    :sparkles: feat: Classe Arara

---
+++ Aula12/src/aula12/Arara.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula12;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Arara extends Ave {
+
+}
