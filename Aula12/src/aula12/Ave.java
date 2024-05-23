e8d0d2248966711c6ee0904f302a8d48547d075a
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:02:55 -0300

    :sparkles: feat: Classe Ave

---
+++ Aula12/src/aula12/Ave.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula12;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Ave extends Animal {
+
+    private String corPena;
+
+    @Override
+    public void locomover() {
+        System.out.println("Voando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo frutas");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de ave");
+    }
+
+    public void fazerNinho() {
+        System.out.println("Constuiu um ninho");
+    }
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getMembros() {
+        return membros;
+    }
+
+    public void setMembros(int membros) {
+        this.membros = membros;
+    }
+
+}
