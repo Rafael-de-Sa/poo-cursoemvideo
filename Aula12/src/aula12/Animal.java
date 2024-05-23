8ee3ec1b102fd2e753d5e31f4fc02e33b43cb93d
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:02:42 -0300

    :sparkles: feat: Classe Animal

---
+++ Aula12/src/aula12/Animal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula12;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public abstract class Animal {
+
+    protected float peso;
+    protected int idade, membros;
+
+    public abstract void locomover();
+
+    public abstract void alimentar();
+
+    public abstract void emitirSom();
+    
+}
