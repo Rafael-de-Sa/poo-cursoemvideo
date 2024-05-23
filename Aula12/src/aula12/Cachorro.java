43a8b47370392a9b64ebdaa06bd16e97a1957d8b
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:22:10 -0300

    :sparkles: feat: Classe Cachorro

---
+++ Aula12/src/aula12/Cachorro.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula12;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Cachorro extends Mamifero {
+
+    public void enterrarOsso() {
+        System.out.println("Enterrando Osso");
+    }
+
+    public void abanarRabo() {
+        System.out.println("Abanando o Rabo");
+    }
+}
