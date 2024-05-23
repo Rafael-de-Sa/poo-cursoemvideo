ff4d8c70d66f23f2926f8a45fdc77e30395d7495
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-22 22:06:15 -0300

    :sparkles: feat: Atrbutos, getters e setters

--- Aula11/src/aula11/Bolsista.java
+++ Aula11/src/aula11/Bolsista.java
@@ -9,5 +9,24 @@ package aula11;
  * @author Rafael de Sá <rafaeldesa170622@gmail.com>
  */
 public class Bolsista extends Aluno {
+
+    private float bolsa;
+
+    public void renovarBolsa() {
+        System.out.println("Renovando a bolsa de +" + this.getNome());
+    }
+
+    @Override
+    public void pagarMensalidade() {
+        System.out.println(this.getNome() + " é bolsista! Pagamento Facilitado.");
+    }
+
+    public float getBolsa() {
+        return bolsa;
+    }
+
+    public void setBolsa(float bolsa) {
+        this.bolsa = bolsa;
+    }
     
 }
