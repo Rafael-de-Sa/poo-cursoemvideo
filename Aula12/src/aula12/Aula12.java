19951b029dc1b46c98cea445b4b60339df9af42e
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 00:23:52 -0300

    :test_tube: test: Testando Ave, Peita, Reptil e Mamifero

--- Aula12/src/aula12/Aula12.java
+++ Aula12/src/aula12/Aula12.java
@@ -14,7 +14,20 @@ public class Aula12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Mamifero m = new Mamifero();
+        Reptil r = new Reptil();
+        Peixe p = new Peixe();
+        Ave a = new Ave();
+
+        m.setPeso(35.3f);
+        m.setCorPelo("Marrom");
+        m.alimentar();
+        m.locomover();
+        m.emitirSom();
+
+        a.locomover();
+        p.locomover();
+        r.locomover();
     }
-    
+
 }
