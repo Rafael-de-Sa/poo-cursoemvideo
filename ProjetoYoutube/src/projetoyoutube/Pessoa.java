571a9d8e1f9240d4eb16ae6cb5f0f7f4d8027170
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-24 14:45:07 -0300

    :sparkles: feat: Classe Pessoa

---
+++ ProjetoYoutube/src/projetoyoutube/Pessoa.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetoyoutube;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public abstract class Pessoa {
+
+    protected String nome, sexo;
+    protected int idade;
+    protected float experiencia;
+
+    public Pessoa(String nome, String sexo, int idade) {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.idade = idade;
+        this.experiencia = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getExperiencia() {
+        return experiencia;
+    }
+
+    public void setExperiencia(float experiencia) {
+        this.experiencia = experiencia;
+    }
+
+    protected void ganharExp() {
+        this.experiencia++;
+    }
+
+}
