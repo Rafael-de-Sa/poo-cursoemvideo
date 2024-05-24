0d75e71b9a05f62af050f18b6b065048a1a43efe
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 22:04:41 -0300

    :sparkles: feat: Classe Mamífero

---
+++ Aula13/src/aula13/Mamifero.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula13;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Mamifero extends Animal {
+
+    protected String corPelo;
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Som de Mamifero");
+    }
+
+}
