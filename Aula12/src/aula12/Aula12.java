e87db446fc920de91d23540aa845f8d046c06c98
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 21:50:08 -0300

    :test_tube: test: Testando reagir

--- Aula12/src/aula12/Aula12.java
+++ Aula12/src/aula12/Aula12.java
@@ -20,7 +20,10 @@ public class Aula12 {
         c.reagir("Vai apanhar");
         c.reagir(11, 45);
         c.reagir(21, 00);
+        c.reagir(true);
         c.reagir(false);
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
     }
     
 }
