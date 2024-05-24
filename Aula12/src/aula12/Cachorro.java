58e6447fe49b1f062ce0747f3497ada367957f16
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 21:44:42 -0300

    :sparkles: feat: Adicionado metodos reagir e rosnar

--- Aula12/src/aula12/Cachorro.java
+++ Aula12/src/aula12/Cachorro.java
@@ -18,9 +18,52 @@ public class Cachorro extends Lobo {
         System.out.println("Abanando o Rabo");
     }
 
+    public void rosnar() {
+        System.out.println("Rosnando");
+    }
+
     @Override
     public void emitirSom() {
         System.out.println("Au! Au! Au!");
+    }
+
+    public void reagir(String frase) {
+        if ("toma comida".equals(frase) || "Olá".equals(frase)) {
+            this.emitirSom();
+            this.abanarRabo();
+        } else {
+            this.rosnar();
+        }
+    }
+
+    public void reagir(int hora, int min) {
+        if (hora < 12) {
+            this.abanarRabo();
+        } else if (hora >= 18) {
+            System.out.println("Ignorar");
+        } else {
+            this.abanarRabo();
+            this.emitirSom();
+        }
+    }
+
+    public void reagir(boolean dono) {
+        if (dono) {
+            this.abanarRabo();
+        } else {
+            this.rosnar();
+            this.emitirSom();
+        }
+    }
+
+    public void reagir(int idade, float peso) {
+        if (idade < 5) {
+            if (peso < 10) {
+                this.abanarRabo();
+            } else {
+                this.emitirSom();
+            }
+        }
     }
 
 }
