a52f4893ebf53a8ae6612a9415aad3f97c50d241
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 21:48:31 -0300

    :test_tube: test: Testando método reagir

--- Aula12/src/aula12/Aula12.java
+++ Aula12/src/aula12/Aula12.java
@@ -14,11 +14,13 @@ public class Aula12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
+        
         Cachorro c = new Cachorro();
         c.reagir("Olá");
         c.reagir("Vai apanhar");
-
+        c.reagir(11, 45);
+        c.reagir(21, 00);
+        c.reagir(false);
     }
-
+    
 }
