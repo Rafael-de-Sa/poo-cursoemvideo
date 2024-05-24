52aebb8d5f6c3829bed4321fd6240043452292af
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-24 14:45:22 -0300

    :sparkles: feat: Classe Gafanhoto

---
+++ ProjetoYoutube/src/projetoyoutube/Gafanhoto.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetoyoutube;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public class Gafanhoto extends Pessoa {
+
+    private String login;
+    private int totAssistido;
+
+    public Gafanhoto(String nome, String sexo, int idade, String login) {
+        super(nome, sexo, idade);
+        this.login = login;
+        this.totAssistido = 0;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getTotAssistido() {
+        return totAssistido;
+    }
+
+    public void setTotAssistido(int totAssistido) {
+        this.totAssistido = totAssistido;
+    }
+
+}
