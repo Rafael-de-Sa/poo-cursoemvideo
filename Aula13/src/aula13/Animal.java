872cad23a6258fa6bed95f768e084bc8da2bc187
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-23 22:04:14 -0300

    :sparkles: feat: Classe Animal

---
+++ Aula13/src/aula13/Animal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula13;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public abstract class Animal {
+
+    protected float peso;
+    protected int idade, membros;
+
+    public abstract void emitirSom();
+}
