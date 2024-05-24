82ead55d74f9349f35ef2e6b9bce45032d1e844c
Rafael de Sá <rafaeldesa170622@gmail.com>
2024-05-24 11:59:10 -0300

    :sparkles: feat: Interface AcoesVideo

---
+++ ProjetoYoutube/src/projetoyoutube/AcoesVideo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package projetoyoutube;
+
+/**
+ *
+ * @author Rafael de Sá <rafaeldesa170622@gmail.com>
+ */
+public interface AcoesVideo {
+
+    public abstract void play();
+
+    public abstract void pause();
+
+    public abstract void like();
+}
